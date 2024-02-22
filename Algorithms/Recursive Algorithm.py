4fd2e6ef8db7e4ed4963fd6a9c6042e3d82b09c1
NateBI <nathanhg5@gmail.com>
2024-02-22 14:40:05 +0000

    editing function

--- Algorithms/Recursive Algorithm.py
+++ Algorithms/Recursive Algorithm.py
@@ -1,31 +1,38 @@
 import pandas as pd
 import numpy as np
+
 V = 4  # the number of nodes / vertices
-INF = 99999 # a large number that will be used for vertices not connected to each other
+INF = 99999  # a large number that will be used for vertices not connected to each other
 # Graph using a list of lists with a dtype of int, referencing the INF when vertices don't connect
 graph = [[0, 5, INF, 10],
          [INF, 0, 3, INF],
          [INF, INF, 0, 1],
          [INF, INF, INF, 0]
          ]
-matrix = pd.DataFrame(graph) # visually more satisfying in dataframe.
+matrix = pd.DataFrame(graph)  # visually more satisfying in dataframe.
 # matrix_two = np.array(graph)
-#print(matrix)
+# print(matrix)
 
 rows = list(map(lambda i: list(map(lambda j: j, i)), graph))
 
 
 def recursive_test(input):
-    if input == 100:
+    # base case
+    if input < 0:
         return 'stop'
-    input += 1
+    # recursive case
     print(input)
-    return recursive_test(input)
+    if input > 1:
+        for i in range(input):
+            print(input, i)
+        else:
+            pass
+    return recursive_test(input-1)
 
-print(recursive_test(-1))
 
+print(recursive_test(4))
 
-
+#
 # for k in range(V): # rows
 #     for i in range(V): # columns
 #         for j in range(V): # values
@@ -34,7 +41,7 @@ print(recursive_test(-1))
 #                              rows[i][k] + rows[k][j]
 #                              )
 #             print_solution = pd.DataFrame(rows)
-#
+# #
 # for i in range(V):
 #     for j in range(V):
 #         if (rows[i][j] == INF):
@@ -44,7 +51,6 @@ print(recursive_test(-1))
 #             if j == V - 1:
 #                 print()
 #
-
 
 
 # Driver's code
