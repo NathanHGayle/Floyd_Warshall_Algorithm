cfed1ec0fc496bd02f20449a89c041952276140f
Nathan Hasting-Gayle <nathanhg5@gmail.com>
2024-02-24 11:20:03 +0000

    Test wasn't configured in Pycharm so re-created to save time

--- Algorithms/test_two_Recursive_Algorithm.py
+++ Algorithms/test_two_Recursive_Algorithm.py
@@ -6,5 +6,5 @@ import numpy as np
 class Test(TestCase):
     def test_create_pairs(self):
         arr = np.array([[1, 2, 3], [4, 5, 6]])
-        product = arr.shape[0] ^ 2
-        self.assertEqual(len(create_pairs(arr)) == product)
+        product = arr.shape[0] ** 2
+        self.assertEqual(len(create_pairs(arr)), product)
