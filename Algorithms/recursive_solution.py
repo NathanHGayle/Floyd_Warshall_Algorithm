c01bc4e5cf3790d4b5bbc7333a5299b9429fe229
Nathan Hasting-Gayle <nathanhg5@gmail.com>
2024-02-24 13:23:17 +0000

    Recursive version of new imperative solution

--- Algorithms/recursive_solution.py
+++ Algorithms/recursive_solution.py
@@ -1,4 +1,7 @@
+import sys
+
 def floyd_recursive(distance):
+    MAX_LENGTH = len(distance)
 
     def helper(start_node, end_node, intermediate):
         # Base case
@@ -6,13 +9,24 @@ def floyd_recursive(distance):
             return 0
         # recursive case
         return min(
-            helper(start_node,end_node,intermediate),
-            helper(start_node,intermediate,intermediate) + helper((intermediate,end_node,intermediate)
-                                                                  )
-
-            # recursive call
-            for intermediate in range(MAX_LENGTH):
-                for start_node in range(MAX_LENGTH):
-                    for end_node in range(MAX_LENGTH)
-
+            distance[start_node][end_node],
+            distance[start_node][intermediate] + distance[intermediate][end_node]
         )
+
+    # Recursive call
+    for intermediate in range(MAX_LENGTH):
+        for start_node in range(MAX_LENGTH):
+            for end_node in range(MAX_LENGTH):
+                distance[start_node][end_node] = helper(start_node, end_node, intermediate)
+
+    print(distance)
+
+NO_PATH = sys.maxsize
+graph = [
+    [0, 7, NO_PATH, 8],
+    [NO_PATH, 0, 5, NO_PATH],
+    [NO_PATH, NO_PATH, 0, 2],
+    [NO_PATH, NO_PATH, NO_PATH, 0]
+]
+
+floyd_recursive(graph)
