f25d5e8ae85bdd8fa7e47e46fbcd1cc18ad41464
Nathan Hasting-Gayle <nathanhg5@gmail.com>
2024-02-24 11:17:07 +0000

    Not firing

--- Algorithms/test_Recursive Algorithm.py
+++ Algorithms/test_Recursive Algorithm.py
@@ -1,7 +1,10 @@
 from unittest import TestCase
-from recur
+from Recursive_Algorithm import create_pairs
 import numpy as np
+
+
 class Test(TestCase):
     def test_create_pairs(self):
         arr = np.array([[1, 2, 3], [4, 5, 6]])
-        self.assertEqual(ra.test_create_pairs(arr)==)
+        product = arr.shape[0] ^ 2
+        self.assertEqual(len(create_pairs(arr)) == product)
