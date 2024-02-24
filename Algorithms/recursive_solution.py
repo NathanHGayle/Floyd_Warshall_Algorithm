e1bedb762e35a11ea3cac3589a8aaa049d05e14e
Nathan Hasting-Gayle <nathanhg5@gmail.com>
2024-02-24 13:17:49 +0000

    Recursive version of new imperative solution

--- Algorithms/recursive_solution.py
+++ Algorithms/recursive_solution.py
@@ -1,44 +1,18 @@
-import pandas as pd
-import numpy as np
+def floyd_recursive(distance):
 
-INF = 99999
-graph = [[0, 5, INF, 10],
-         [INF, 0, 3, INF],
-         [INF, INF, 0, 1],
-         [INF, INF, INF, 0]
-         ]
-matrix = np.array(graph)
+    def helper(start_node, end_node, intermediate):
+        # Base case
+        if start_node == end_node:
+            return 0
+        # recursive case
+        return min(
+            helper(start_node,end_node,intermediate),
+            helper(start_node,intermediate,intermediate) + helper((intermediate,end_node,intermediate)
+                                                                  )
 
+            # recursive call
+            for intermediate in range(MAX_LENGTH):
+                for start_node in range(MAX_LENGTH):
+                    for end_node in range(MAX_LENGTH)
 
-# establish pairs - include value k, i , j
-def create_pairs(matrix):
-    pairs = []
-    V = range(matrix.shape[0])
-    for k in V:
-        for i in V:
-            pairs.append([k, i, matrix[k][i]])
-    return pairs
-
-
-def floydwarshall(matrix):
-    # Create a separate matrix for updated distances
-    updated_matrix = np.copy(matrix)
-    pairs = create_pairs(updated_matrix)
-    # Loop through each pair to find the matching destinations
-    for pair in pairs:
-        # membership test
-        for loop in pairs:
-            if loop == pair:
-                pass
-            else:
-                destination_match = 'yes' if loop[1] == pair[1] and loop[0] != pair[0] else 'no'
-                if destination_match == 'yes':
-                    # If alternative route is shorter update this in the updated matrix
-                    if matrix[loop[0]][pair[0]] + pair[2] < updated_matrix[loop[0]][loop[1]]:
-                        updated_matrix[loop[0]][loop[1]] = matrix[loop[0]][pair[0]] + pair[2]
-    return updated_matrix
-
-
-updated_matrix = floydwarshall(matrix)
-print("Following matrix shows the shortest distances between every pair of vertices")
-print(updated_matrix)
+        )
